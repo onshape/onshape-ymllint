752222de3396f39d4289652ac2aa12def5b33b37
Haoyuan Lu <halu@ptc.com>
2025-06-27 13:00:29 -0400

    Fixed parameter parsing

--- scripts/post-code-review.js
+++ scripts/post-code-review.js
@@ -4,7 +4,7 @@ export default async function postCodeReview (github, context, filepath, line, c
     pull_number: context.issue.number,
     commit_id: context.payload.pull_request.head.sha,
     path: filepath,
-    position: line,
+    position: parseInt(line, 10),
     body: commentBody,
   });
 }
